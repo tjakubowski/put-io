c463ebf888a88bd00bd581d099543e2df01dd8a4
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2020-10-26 02:30:26 +0100

    Add basic client communication in TcpServerAsync

--- lab2/ServerLibrary/TcpServerAsync.cs
+++ lab2/ServerLibrary/TcpServerAsync.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,13 +11,61 @@ namespace ServerLibrary
 {
     public class TcpServerAsync : TcpServer
     {
+        private delegate void HandleDataTransmissionDelegate(NetworkStream stream);
+
         public TcpServerAsync(IPAddress ipAddress, int port) : base(ipAddress, port)
         {
         }
 
         public override void Start()
         {
-            throw new NotImplementedException();
+            StartListening();
+            AcceptClient();
         }
+
+        protected override void AcceptClient()
+        {
+            while (true)
+            {
+                TcpClient tcpClient = _listener.AcceptTcpClient();
+                NetworkStream stream = tcpClient.GetStream();
+
+                Console.WriteLine($"Client connected");
+
+                HandleDataTransmissionDelegate handleDataTransmissionDelegate = HandleDataTransmission;
+
+                handleDataTransmissionDelegate.BeginInvoke(stream, CloseClientConnection, tcpClient);
+            }
+
+        }
+
+        protected override void HandleDataTransmission(NetworkStream stream)
+        {
+            var buffer = new byte[BufferSize];
+
+            while(true)
+            {
+                try
+                {
+                    var messageSize = stream.Read(buffer, 0, BufferSize);
+                    Console.WriteLine(System.Text.Encoding.ASCII.GetString(buffer, 0, messageSize));
+                    stream.Write(buffer, 0, messageSize);
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+
+        void CloseClientConnection(IAsyncResult result)
+        {
+            TcpClient client = result as TcpClient;
+            client.Close();
+
+            Console.WriteLine($"Client disconnected");
+        }
+
+
     }
 }
