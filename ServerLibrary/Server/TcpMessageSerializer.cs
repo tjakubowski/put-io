381ccfcc93b6ec3f136601656ebbaad954a97c7b
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2020-11-10 22:23:41 +0100

    Add TcpMessageSerializer

---
+++ ServerLibrary/Server/TcpMessageSerializer.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ServerLibrary.Server
+{
+    public static class TcpMessageSerializer
+    {
+        public static TcpMessage Serialize(object anySerializableObject)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                (new BinaryFormatter()).Serialize(memoryStream, anySerializableObject);
+                return new TcpMessage { Data = memoryStream.ToArray() };
+            }
+        }
+
+        public static object Deserialize(TcpMessage tcpMessage)
+        {
+            using (var memoryStream = new MemoryStream(tcpMessage.Data))
+                return (new BinaryFormatter()).Deserialize(memoryStream);
+        }
+    }
+}
