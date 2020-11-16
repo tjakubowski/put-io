dbe9b1e5d82550f84ef22717993d71d9a3e9c609
Michał Balcewicz <mxbalcewicz@gmail.com>
2020-11-16 03:09:36 +0100

    edited client.cs, #TODO, base for client connection and sending serialized messages

--- ServerLibrary/Client/Client.cs
+++ ServerLibrary/Client/Client.cs
@@ -5,63 +5,54 @@ using System.Net.Sockets;
 using System.Text;
 using System.IO;
 using System.Threading.Tasks;
+using ServerLibrary.Server;
+using ServerLibrary.Server.Messages;
 
 namespace ServerLibrary.Client
 {
     class Client
     {
-        public static void StartClient(String server, String message)
+
+        protected Client()
+        {
+            TcpClient tcpclient = new TcpClient();
+        }
+
+        public void StartClient()
         {
             try
             {
-                // Create a TcpClient.
-                // Note, for this client to work you need to have a TcpServer
-                // connected to the same address as specified by the server, port
-                // combination.
-                Int32 port = 13000;
-                TcpClient client = new TcpClient(server, port);
+                TcpClient tcpclient = new TcpClient();
+                tcpclient.Connect("127.0.0.1", 5000);
 
-                // Translate the passed message into ASCII and store it as a Byte array.
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
 
-                // Get a client stream for reading and writing.
-                //  Stream stream = client.GetStream();
-
-                NetworkStream stream = client.GetStream();
-
-                // Send the message to the connected TcpServer.
-                stream.Write(data, 0, data.Length);
-
-                Console.WriteLine("Sent: {0}", message);
-
-                // Receive the TcpServer.response.
-
-                // Buffer to store the response bytes.
-                data = new Byte[256];
-
-                // String to store the response ASCII representation.
-                String responseData = String.Empty;
-
-                // Read the first batch of the TcpServer response bytes.
-                Int32 bytes = stream.Read(data, 0, data.Length);
-                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Received: {0}", responseData);
-
-                // Close everything.
-                stream.Close();
-                client.Close();
+                tcpclient.Close();
             }
-            catch (ArgumentNullException e)
+
+            catch (Exception e)
             {
-                Console.WriteLine("ArgumentNullException: {0}", e);
+               //
             }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
+        }
+
+        public void SendLoginData(string log, string pass)
+        {
+            try {
+                AuthenticationForm form = new AuthenticationForm(log, pass);
+                TcpMessage login_msg = MessageSerializer.Serialize(form);
+                Stream.Write(login_msg);
+            }
+            catch (Exception e) {
+                Console.WriteLine(String.Format("Error: {0}", e.StackTrace)); 
             }
 
-            Console.WriteLine("\n Press Enter to continue...");
-            Console.Read();
+        }
+
+        public static void SendMessage(int ch_id, string msg)
+        {
+            MessageForm form = new MessageForm(ch_id, msg);
+            TcpMessage message = MessageSerializer.Serialize(form);
+            Stream.Write(message);
         }
     }
 }
