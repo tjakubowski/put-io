fc92cc5f482805f659929ff8a7bec4cec45f619c
Michał Balcewicz <mxbalcewicz@gmail.com>
2020-11-17 01:44:26 +0100

    changed client loop to start from login, added LoginWindow.xaml

--- App/MainWindow.xaml.cs
+++ App/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServerLibrary.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace App
     {
         public MainWindow()
         {
+            Client client = new Client();
             InitializeComponent();
         }
     }
--- ServerLibrary/Client/Client.cs
+++ ServerLibrary/Client/Client.cs
@@ -10,49 +10,125 @@ using ServerLibrary.Server.Messages;
 
 namespace ServerLibrary.Client
 {
-    class Client
+    public class Client
     {
+        protected TcpClient tcpC;
+        protected Stream stream;
+        protected bool loggedIn;
+        protected bool conn;
+        private byte[] readBytes;
 
-        protected Client()
+        public Client()
         {
-            TcpClient tcpclient = new TcpClient();
+            tcpC = new TcpClient("127.0.0.1", 2048);
+            stream = tcpC.GetStream();
+            loggedIn = false;
+            conn = false;
         }
 
         public void StartClient()
         {
             try
             {
-                TcpClient tcpclient = new TcpClient();
-                tcpclient.Connect("127.0.0.1", 5000);
-
-
-                tcpclient.Close();
+                RunSession();
+                tcpC.Close();
             }
 
             catch (Exception e)
             {
-               //
+                throw e;
             }
         }
+
+        public void RunSession()
+        {
+            Task.Run(() =>
+            {
+                HandleClient();
+                //TODO close.client()
+            });
+        }
+
+        protected void HandleClient()
+        {
+            while (!loggedIn)
+            {
+                try
+                {
+                    //#TO DO stream.Read();
+                    var request = MessageSerializer.Deserialize(new TcpMessage(readBytes));
+                    //if ('OK')     #TO DO
+                    //{
+                    //    loggedIn = true;
+                    //}
+                }catch(ArgumentNullException)
+                {
+                    return;
+                }
+            }
+
+
+            if (loggedIn)
+            {
+                while (this.conn == true)
+                {
+                    try
+                    {
+                        
+                        //TcpMessage msg = stream.Read();
+                        //var request = MessageSerializer.Deserialize(new TcpMessage(readBytes));
+
+                        //if (request is AuthenticationForm authenticationForm)
+                        //{
+                        //    switch (authenticationForm.AuthenticationType)
+                        //    {
+                        //        case AuthenticationType.Login:
+                        //            Login(session, authenticationForm);
+                        //            break;
+                        //        case AuthenticationType.Register:
+                        //            Register(session, authenticationForm);
+                        //            break;
+                        //    }
+                        //}
+
+                        //if (session.User == null)
+                        //    continue;
+
+                        //if (request is ChangePasswordForm changePasswordForm)
+                        //{
+                        //    ChangePassword(session, changePasswordForm);
+                        //}
+                    }
+                    catch (ArgumentNullException)
+                    {
+                        return;
+                    }
+                }
+            }
+
+        }
+
 
         public void SendLoginData(string log, string pass)
         {
-            try {
+            try
+            {
                 AuthenticationForm form = new AuthenticationForm(log, pass);
                 TcpMessage login_msg = MessageSerializer.Serialize(form);
-                Stream.Write(login_msg);
+                stream.Write(login_msg.Data, 0, login_msg.Data.Length);
             }
-            catch (Exception e) {
-                Console.WriteLine(String.Format("Error: {0}", e.StackTrace)); 
+            catch (Exception e)
+            {
+                Console.WriteLine(String.Format("Error: {0}", e.StackTrace));
             }
 
         }
 
-        public static void SendMessage(int ch_id, string msg)
+        public void SendMessage(int ch_id, string msg)
         {
             MessageForm form = new MessageForm(ch_id, msg);
             TcpMessage message = MessageSerializer.Serialize(form);
-            Stream.Write(message);
+            stream.Write(message.Data, 0, message.Data.Length);
         }
     }
 }
